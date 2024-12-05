a56b0c9c7594675e1925724ec52fe0e95d54db28
Kyrylo Ulianov <kirillulianow@gmail.com>
2024-12-06 04:24:57 +0430

    changed output format

--- radix_sort/radix_sort.c
+++ radix_sort/radix_sort.c
@@ -84,11 +84,11 @@ void radix_sort_dyn_arr(DynArr *arr, SortOrder order)
 
   if (order == ASCENDING)
   {
-    printf("\n\033[32m\033[0m Sorting order: \033[32mAscending\033[0m\n");
+    printf("\n\033[32m\033[0m Sorting order: \033[32masc\033[0m\n");
   }
   else
   {
-    printf("\n\033[32m\033[0m Sorting order: \033[32mDescending\033[0m\n");
+    printf("\n\033[32m\033[0m Sorting order: \033[32mdesc\033[0m\n");
   }
 
   printf("\033[32m󰓡\033[0m Number of swaps: \033[32m%d\033[0m\n", stats.swaps);
