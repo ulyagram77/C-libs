cb503aef6d0fefaa0dcdd9a240821f59fd9729ff
Kyrylo Ulianov <kirillulianow@gmail.com>
2024-12-06 03:08:09 +0430

    added glib

--- tree_sort/tree_sort.c
+++ tree_sort/tree_sort.c
@@ -1,92 +1,37 @@
-#include "tree_sort.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include "tree_sort.h"
 
-int compare(void *a, void *b, size_t elem_size)
+static gint compare_elements(const void *a, const void *b, gpointer user_data)
 {
+  size_t elem_size = *(size_t *)user_data;
   return memcmp(a, b, elem_size);
 }
 
-TreeNode *insert_to_tree(TreeNode *root, void *data, size_t elem_size, int *comparison_count, int *move_count)
+static void add_to_array(gpointer key, gpointer value, gpointer user_data)
 {
-  if (root == NULL)
-  {
-    TreeNode *new_node = malloc(sizeof(TreeNode));
-    new_node->data = malloc(elem_size);
-    memcpy(new_node->data, data, elem_size);
-    new_node->left = new_node->right = NULL;
-    (*move_count)++;
-    return new_node;
-  }
-
-  (*comparison_count)++;
-  int cmp = compare(data, root->data, elem_size);
-  if (cmp < 0)
-  {
-    root->left = insert_to_tree(root->left, data, elem_size, comparison_count, move_count);
-  }
-  else if (cmp > 0)
-  {
-    root->right = insert_to_tree(root->right, data, elem_size, comparison_count, move_count);
-  }
-
-  return root;
+  DynArr *arr = (DynArr *)user_data;
+  push_dyn_arr(arr, key);
 }
 
-void inorder_tree(TreeNode *root, void *arr, size_t elem_size, size_t *index, int *move_count)
+void sort_with_gtree(DynArr *arr)
 {
-  TreeNode **stack = malloc(1000 * sizeof(TreeNode *));
-  int top = -1;
-
-  TreeNode *current = root;
-
-  while (current != NULL || top >= 0)
-  {
-    while (current != NULL)
-    {
-      stack[++top] = current;
-      current = current->left;
-    }
-
-    current = stack[top--];
-
-    memcpy((char *)arr + (*index) * elem_size, current->data, elem_size);
-    (*index)++;
-    (*move_count)++;
-
-    current = current->right;
-  }
-
-  free(stack);
-}
-
-void destroy_tree(TreeNode *root)
-{
-  if (root == NULL)
+  if (arr->size == 0)
     return;
-  destroy_tree(root->left);
-  destroy_tree(root->right);
-  free(root->data);
-  free(root);
-}
 
-void sort_with_tree(DynArr *arr)
-{
-  int comparison_count = 0, move_count = 0;
-  TreeNode *root = NULL;
+  GTree *tree = g_tree_new_full(compare_elements, &arr->elem_size, free, NULL);
 
   for (size_t i = 0; i < arr->size; i++)
   {
-    void *element = (char *)arr->data + i * arr->elem_size;
-    root = insert_to_tree(root, element, arr->elem_size, &comparison_count, &move_count);
+    void *element = malloc(arr->elem_size);
+    memcpy(element, (char *)arr->data + i * arr->elem_size, arr->elem_size);
+    g_tree_insert(tree, element, NULL);
   }
 
-  size_t index = 0;
-  inorder_tree(root, arr->data, arr->elem_size, &index, &move_count);
+  arr->size = 0;
 
-  printf("\033[32m󰓡\033[0m Number of swaps: \033[32m%d\033[0m\n", move_count);
-  printf("\033[32m\033[0m Number of comparisons: \033[32m%d\033[0m\n", comparison_count);
+  g_tree_foreach(tree, add_to_array, arr);
 
-  destroy_tree(root);
+  g_tree_destroy(tree);
 }
--- tree_sort/tree_sort.h
+++ tree_sort/tree_sort.h
@@ -1,16 +1,9 @@
 #ifndef TREE_SORT_H
 #define TREE_SORT_H
 
-#include <stddef.h>
+#include <glib.h>
 #include "dyn_arr.h"
 
-typedef struct TreeNode
-{
-  void *data;
-  struct TreeNode *left;
-  struct TreeNode *right;
-} TreeNode;
-
-void sort_with_tree(DynArr *arr);
+void sort_with_gtree(DynArr *arr);
 
 #endif
